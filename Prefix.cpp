3a8ff1580d9041fe031ae0269152325e7e54acca
riyakhanna25 <104088194+riyakhanna25@users.noreply.github.com>
2022-04-20 22:47:01 +0530

    Update Prefix.cpp

--- Prefix.cpp
+++ Prefix.cpp
@@ -1,3 +1,20 @@
+Aim: Write a program in C/C++ or Java to generate Intermediate Code (Prefix Expression)from given syntax tree.
+Algorithm :
+
+1.	First, reverse the given infix expression.
+2.	Scan the characters one by one.
+3.	If the character is an operand, copy it to the prefix notation output.
+4.	If the character is a closing parenthesis, then push it to the stack.
+5.	If the character is an opening parenthesis, pop the elements in the stack until we find the corresponding closing parenthesis.
+6.	If the character scanned is an operator
+
+•	If the operator has precedence greater than or equal to the top of the stack, push the operator to the stack.
+•	If the operator has precedence lesser than the top of the stack, pop the operator and output it to the prefix notation output and then check the above condition again with the new top of the stack.
+
+7.	After all the characters are scanned, reverse the prefix notation output.
+
+
+
 #define SIZE 50 /* Size of Stack */
 #include<string.h>
 #include<stdio.h>
