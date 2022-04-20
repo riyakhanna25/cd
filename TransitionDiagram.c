c62c97869a4c5f8f47680450813b84fefdbbb431
riyakhanna25 <104088194+riyakhanna25@users.noreply.github.com>
2022-04-20 22:32:35 +0530

    Update TransitionDiagram.c

--- TransitionDiagram.c
+++ TransitionDiagram.c
@@ -1,3 +1,19 @@
+Aim: Write a program in C/C++ to show the transition table from a given transition diagram.
+
+Algorithm:
+1.	Start
+2.	Enter the number of states.
+3.	Enter the number of input variables.
+4.	Enter the state and its information.
+5.	Enter the input variables.
+6.	Enter the transition function information i.e., transition value from a state with an input variable.
+7.	Show the Transition Table.
+8.	Stop
+
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 struct setStates
